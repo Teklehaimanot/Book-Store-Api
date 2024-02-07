060a2b1bc528a11c0166976314debc63242ee5f1
teklehaimanot <teklehaimanotbelete@gmail.com>
2024-02-07 07:43:48 +0300

    setting up swager documentation with my cutomer apis

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,23 @@ import express from "express";
 import bookRoutes from "./route/bookRoutes";
 import cutomerRoutes from "./route/cutomerRoute";
 import orderRoutes from "./route/orderRoutes";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
 require("dotenv/config");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const swaggerCustomerDocument = YAML.load("./swagger-customer.yaml");
+
 app.use(express.json());
+
 app.use("/api/v1/books", bookRoutes);
 app.use("/api/v1/customers", cutomerRoutes);
 app.use("/api/v1/orders", orderRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerCustomerDocument));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
